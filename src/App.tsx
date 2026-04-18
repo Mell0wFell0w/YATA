caec46e98c44a5d61b18c003a68a03cb1e125504
Matt Cooper <mccoopton@gmail.com>
2026-04-18 15:36:57 -0600

    Basic scaffold Vite + React TS with Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-black/95">YATA</h1>
+    </main>
+  )
+}
+
+export default App
